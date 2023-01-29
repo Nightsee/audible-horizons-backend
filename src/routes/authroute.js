7fbc4d5f4e2d2b365937fbcb8638a0a1ca41c4d6
Nightsee <younes.naitsi.2002e@gmail.com>
2023-01-29 21:57:58 +0100

    hh

--- src/routes/authroute.js
+++ src/routes/authroute.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+
 const router = express.Router()
 const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs');
